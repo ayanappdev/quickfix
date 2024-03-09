6d31fe1d1e88018839974765c24209ac288faf82
AnasKhan1506 <akanaskhan1506@gmail.com>
2024-03-08 16:51:10 -0800

    login ui setup

--- lib/Screens/login_Screen.dart
+++ lib/Screens/login_Screen.dart
@@ -11,19 +11,28 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(157, 224, 224, 224),
+        backgroundColor: Colors.grey[200],
         body: SafeArea(
           child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const SizedBox(
-                  height: 10,
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Image.asset(
+                    "assets/images/logo.PNG",
+                    height: 300,
+                    width: 300,
+                  ),
                 ),
-                const Text(
-                  "Welcome To QuickFix",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  child: const Text(
+                    "Welcome To QuickFix",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
                   ),
                 ),
                 const SizedBox(
@@ -34,22 +43,96 @@ class _LoginScreenState extends State<LoginScreen> {
                   style: TextStyle(fontSize: 20),
                 ),
                 const SizedBox(
-                  height: 20,
+                  height: 60,
                 ),
                 Padding(
                   padding: EdgeInsets.symmetric(horizontal: 25),
                   child: Container(
                     decoration: BoxDecoration(
-                        color: Colors.grey,
-                        border: Border.all(color: Colors.white),
+                        color: Color.fromARGB(78, 27, 111, 97),
                         borderRadius: BorderRadius.circular(12)),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Email",
+                        ),
                       ),
                     ),
                   ),
                 ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(78, 27, 111, 97),
+                        borderRadius: BorderRadius.circular(12)),
+                    child: const Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: const TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Password",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Color.fromARGB(255, 27, 111, 97),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Sign In",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Not a member?",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "Register Now",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 58, 232, 203),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 150,
+                )
               ],
             ),
           ),
