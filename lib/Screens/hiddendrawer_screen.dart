9314c45ed69ff73c6e16241c8042a40b10a963c9
AnasKhan1506 <akanaskhan1506@gmail.com>
2024-03-22 17:16:19 -0700

    creating drawer for homescreen

---
+++ lib/Screens/hiddendrawer_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
+import 'package:quickfix/Screens/home_screen.dart';
+
+class HiddenDrawer extends StatefulWidget {
+  const HiddenDrawer({super.key});
+
+  @override
+  State<HiddenDrawer> createState() => _HiddenDrawerState();
+}
+
+class _HiddenDrawerState extends State<HiddenDrawer> {
+  List<ScreenHiddenDrawer> _pages = [];
+  @override
+  void initState() {
+    super.initState();
+    _pages = [
+      ScreenHiddenDrawer(
+        ItemHiddenMenu(
+            name: "HomePage",
+            baseStyle: TextStyle(),
+            selectedStyle: TextStyle()),
+        HomeScreen(),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return HiddenDrawerMenu(
+      screens: _pages,
+      backgroundColorMenu: Color.fromARGB(255, 75, 202, 134),
+      initPositionSelected: 0,
+    );
+  }
+}
